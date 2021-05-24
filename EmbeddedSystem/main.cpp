e830f58c2a255707f6ae46c91f34e8c2730281d5
Mario-projects-dev <m.vitek.mv@gmail.com>
2021-05-24 20:48:18 +0200

    first commit

---
+++ EmbeddedSystem/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * EmbeddedSystem.cpp
+ *
+ * Created: 24. 5. 2021 20:03:35
+ * Author : Mario
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(int argc, char **argv)
+{
+	//Set PORTB1 as output
+	DDRB=0b00000010;
+
+	while(1)
+	{
+		//Set PB1=High(+5v), this will turn on the LED
+		PORTB|=0b00000010;
+		_delay_ms(500); // wait for half second, then turn it off
+
+		//Set PB1=Low(GND)
+		PORTB&=0b11111101;
+		_delay_ms(500);
+	}
+		return 0;
+}
+
